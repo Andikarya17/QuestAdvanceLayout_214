393159cae64e333f80c8b93a2fedfe659d567bc9
Andikarya17 <andikaryaputra17@gmail.com>
2025-10-23 09:15:17 +0700

    Initial commit

---
+++ app/src/main/java/com/example/questadvancelayout_214/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.questadvancelayout_214
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.questadvancelayout_214.ui.ActivitasPertama
+import com.example.questadvancelayout_214.ui.theme.QuestAdvanceLayout_214Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QuestAdvanceLayout_214Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ActivitasPertama(
+
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QuestAdvanceLayout_214Theme {
+        Greeting("Android")
+    }
+}
